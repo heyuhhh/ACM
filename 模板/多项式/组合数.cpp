8995e0fe2c4791d792d0f24a19a2bc04d11aa6a8
heyuhhh <2468861298@qq.com>
2019-12-14 21:32:22 +0800

    组合数

---
+++ 模板/多项式/组合数.cpp
@@ -0,0 +1,19 @@
+int fac[N], inv[N];
+ll qpow(ll a, ll b) {
+    ll ans = 1;
+    while(b) {
+        if(b & 1) ans = ans * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;   
+    }
+    return ans;   
+}
+int C(int n, int m) {
+    return 1ll * fac[n] * inv[m] % MOD * inv[n - m] % MOD;
+}
+void init() {
+    fac[0] = 1;
+    for(int i = 1; i < N; i++) fac[i] = 1ll * fac[i - 1] * i % MOD;
+    inv[N - 1] = qpow(fac[N - 1], MOD - 2);
+    for(int i = N - 2; i >= 0; i--) inv[i] = 1ll * inv[i + 1] * (i + 1) % MOD;
+}
