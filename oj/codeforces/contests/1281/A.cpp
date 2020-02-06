2f11244135b50a4a90709bf88280ce611fa33658
heyuhhh <2468861298@qq.com>
2020-02-06 11:22:16 +0800

    Codeforces Round #607 (Div. 2)

---
+++ oj/codeforces/contests/1281/A.cpp
@@ -0,0 +1,55 @@
+/*
+ * Author:  heyuhhh
+ * Created Time:  2019/12/15 13:06:20
+ */
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#define MP make_pair
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define INF 0x3f3f3f3f
+#define Local
+#ifdef Local
+  #define dbg(args...) do { cout << #args << " -> "; err(args); } while (0)
+  void err() { std::cout << '\n'; }
+  template<typename T, typename...Args>
+  void err(T a, Args...args) { std::cout << a << ' '; err(args...); }
+#else
+  #define dbg(...)
+#endif
+void pt() {std::cout << '\n'; }
+template<typename T, typename...Args>
+void pt(T a, Args...args) {std::cout << a << ' '; pt(args...); }
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+//head
+const int N = 1000 + 5;
+
+char s[N];
+
+void run(){
+    cin >> (s + 1);
+    int n = strlen(s + 1);
+    if(s[n] == 'o') cout << "FILIPINO" << '\n';
+    else if(s[n] == 'u') cout << "JAPANESE" << '\n';
+    else cout << "KOREAN" << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << fixed << setprecision(20);
+    int T; cin >> T;
+    while(T--) run();
+    return 0;
+}
