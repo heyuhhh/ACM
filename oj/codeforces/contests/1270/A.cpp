61b56699bfc38640275a05f802b2069d7801d2a8
heyuhhh <2468861298@qq.com>
2020-02-06 11:07:17 +0800

    Initial Commit

---
+++ oj/codeforces/contests/1270/A.cpp
@@ -0,0 +1,59 @@
+/*
+ * Author:  heyuhhh
+ * Created Time:  2019/12/29 22:06:52
+ */
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#define MP make_pair
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define INF 0x3f3f3f3f
+#define Local
+#ifdef Local
+  #define dbg(args...) do { cout << #args << " -> "; err(args); } while (0)
+  void err() { std::cout << '\n'; }
+  template<typename T, typename...Args>
+  void err(T a, Args...args) { std::cout << a << ' '; err(args...); }
+#else
+  #define dbg(...)
+#endif
+void pt() {std::cout << '\n'; }
+template<typename T, typename...Args>
+void pt(T a, Args...args) {std::cout << a << ' '; pt(args...); }
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+//head
+const int N = 1e5 + 5;
+
+void run(){
+    int n, k1, k2; cin >> n >> k1 >> k2;
+    int f = 0;
+    for(int i = 1; i <= k1; i++) {
+        int x; cin >> x;
+    }
+    for(int i = 1; i <= k2; i++) { 
+        int x; cin >> x;
+        if(x == n) f = 1;
+    }
+    if(f) cout << "NO" << '\n';
+    else cout << "YES" << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << fixed << setprecision(20);
+    int T; cin >> T;
+    while(T--) run();
+    return 0;
+}
