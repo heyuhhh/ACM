ec1aa9e1dcab2f1d7280f81c28e9f1c90cece4e1
heyuhhh <2468861298@qq.com>
2020-03-25 16:23:24 +0800

    P3803_vector.cpp

---
+++ oj/luogu/P3803_vector.cpp
@@ -0,0 +1,98 @@
+/*
+ * Author:  heyuhhh
+ * Created Time:  2020/3/25 16:07:29
+ */
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <assert.h>
+#define MP make_pair
+#define fi first
+#define se second
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define INF 0x3f3f3f3f
+#define Local
+#ifdef Local
+  #define dbg(args...) do { cout << #args << " -> "; err(args); } while (0)
+  void err() { std::cout << '\n'; }
+  template<typename T, typename...Args>
+  void err(T a, Args...args) { std::cout << a << ' '; err(args...); }
+  template <template<typename...> class T, typename t, typename... A> 
+  void err(const T <t> &arg, const A&... args) {
+  for (auto &v : arg) std::cout << v << ' '; err(args...); }
+#else
+  #define dbg(...)
+#endif
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+//head
+const int MOD = 998244353, inv2 = (MOD + 1) >> 1;
+const int N = 4e6 + 5; //四倍空间
+typedef vector <int> Poly;
+#define ri register int
+#define cs const
+inline int add(int a, int b) {return a + b >= MOD ? a + b - MOD : a + b;}
+inline int dec(int a, int b) {return a < b ? a - b + MOD : a - b;}
+inline int mul(int a, int b) {return 1ll * a * b % MOD;}
+inline int qpow(int a, int b) {int res = 1; while(b) {if(b & 1) res = mul(res, a); a = mul(a, a); b >>= 1;} return res;}
+int rev[N], inv[N];
+inline void init() {
+    inv[0] = inv[1] = 1;
+    for(ri i = 2; i < N; i++) inv[i] = mul(inv[MOD % i], MOD - MOD / i);
+}      
+inline void init_rev(int len) {
+    for(ri i = 0; i < len; i++) rev[i] = rev[i >> 1] >> 1 | ((i & 1) * (len >> 1));   
+}
+inline void NTT(Poly &a, int n, int op) {
+    for(ri i = 0; i < n; i++) if(i < rev[i]) swap(a[i], a[rev[i]]);
+    for(ri i = 1; i < n; i <<= 1) {
+        int wn = qpow(op == -1 ? (MOD + 1) / 3 : 3, (MOD - 1) / i / 2);
+        for(ri j = 0; j < n; j += (i << 1)) {
+            for(ri k = 0, x, y, w = 1; k < i; ++k, w = mul(w, wn)) {
+                x = a[j + k], y = mul(w, a[j + k + i]);
+                a[j + k] = add(x, y);
+                a[j + k + i] = dec(x, y);
+            }   
+        }
+    }   
+    if(op == -1) for(ri i = 0; i < n; i++) a[i] = mul(a[i], inv[n]);
+}
+inline Poly operator * (Poly a, Poly b) {
+    int n = sz(a), m = sz(b), l = 1;
+    while(l < n + m - 1) l <<= 1;
+    init_rev(l);
+    a.resize(l), NTT(a, l, 1);
+    b.resize(l), NTT(b, l, 1);
+    for(ri i = 0; i < l; i++) a[i] = mul(a[i], b[i]);
+    NTT(a, l, -1);
+    a.resize(n + m - 1);
+    return a;
+}
+
+void run() {
+    init();
+    int n, m;
+    cin >> n >> m;
+    Poly a(n + 1), b(m + 1);
+    for(int i = 0; i <= n; i++) cin >> a[i];
+    for(int i = 0; i <= m; i++) cin >> b[i];
+    Poly c = a * b;
+    for(auto it : c) cout << it << ' ';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << fixed << setprecision(20);
+    run();
+    return 0;
+}
