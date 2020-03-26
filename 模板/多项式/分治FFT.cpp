160a358521c5470ab94f79c96e02f135a519616e
heyuhhh <2468861298@qq.com>
2020-03-26 11:17:19 +0800

    分治FFT

---
+++ 模板/多项式/分治FFT.cpp
@@ -0,0 +1,57 @@
+const int MOD = 998244353, inv2 = (MOD + 1) >> 1;
+const int N = 4e5 + 5; //四倍空间
+typedef vector <int> Poly;
+#define ri register int
+#define cs const
+inline int add(int a, int b) {return a + b >= MOD ? a + b - MOD : a + b;}
+inline int dec(int a, int b) {return a < b ? a - b + MOD : a - b;}
+inline int mul(int a, int b) {return 1ll * a * b % MOD;}
+inline int qpow(int a, int b) {int res = 1; while(b) {if(b & 1) res = mul(res, a); a = mul(a, a); b >>= 1;} return res;}
+int rev[N], inv[N];
+inline void init() {
+    inv[0] = inv[1] = 1;
+    for(ri i = 2; i < N; i++) inv[i] = mul(inv[MOD % i], MOD - MOD / i);
+}      
+inline void init_rev(int len) {
+    for(ri i = 0; i < len; i++) rev[i] = rev[i >> 1] >> 1 | ((i & 1) * (len >> 1));   
+}
+inline void NTT(Poly &a, int n, int op) {
+    for(ri i = 0; i < n; i++) if(i < rev[i]) swap(a[i], a[rev[i]]);
+    for(ri i = 1; i < n; i <<= 1) {
+        int wn = qpow(op == -1 ? (MOD + 1) / 3 : 3, (MOD - 1) / i / 2);
+        for(ri j = 0; j < n; j += (i << 1)) {
+            for(ri k = 0, x, y, w = 1; k < i; ++k, w = mul(w, wn)) {
+                x = a[j + k], y = mul(w, a[j + k + i]);
+                a[j + k] = add(x, y);
+                a[j + k + i] = dec(x, y);
+            }   
+        }
+    }   
+    if(op == -1) for(ri i = 0; i < n; i++) a[i] = mul(a[i], inv[n]);
+}
+inline Poly operator * (Poly a, Poly b) {
+    int n = sz(a), m = sz(b), l = 1;
+    while(l < n + m - 1) l <<= 1;
+    init_rev(l);
+    a.resize(l), NTT(a, l, 1);
+    b.resize(l), NTT(b, l, 1);
+    for(ri i = 0; i < l; i++) a[i] = mul(a[i], b[i]);
+    NTT(a, l, -1);
+    a.resize(n + m - 1);
+    return a;
+}
+int n;
+Poly f, g;
+//f_i=\sum_{j=0}^{i-1}f_j*g_{i-j},f[0]=1
+void solve(int l, int r) {
+    if(l >= r) return;
+    int mid = (l + r) >> 1;
+    solve(l, mid);
+    Poly a(r - l), b(r - l);
+    Poly c = f;
+    for(int i = mid + 1; i <= r; i++) c[i] = 0;
+    for(int i = 0; i < r - l; i++) a[i] = c[i + l], b[i] = g[i + 1];
+    c = a * b;
+    for(int i = mid + 1; i <= r; i++) f[i] = (f[i] + c[i - l - 1]) % MOD;
+    solve(mid + 1, r);
+}
