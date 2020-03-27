58ab9da9480d35c6446f4f5c9e1b57aa78cdacca
heyuhhh <2468861298@qq.com>
2020-03-27 22:54:56 +0800

    能够求解最小正整数解x

--- 模板/数论/exgcd.cpp
+++ 模板/数论/exgcd.cpp
@@ -8,3 +8,15 @@ void exgcd(ll a, ll b, ll &x, ll &y) {
     x = y;
     y = z - y * (a / b);
 } 
+//求解最小正整数解
+//x=c*x_0+k*b,y=c*y_0-k*a
+ll calc(ll a, ll b, ll c) {
+    ll x, y;
+    ll g = __gcd(a, b);
+    if(c % g != 0) return -1;
+    a /= g, b /= g, c /= g;
+    exgcd(a, b, x, y);
+    x *= c;
+    x = (x % b + b) % b;
+    return x;
+}
