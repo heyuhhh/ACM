0f9beced8b7588f26dc00391b1e76f77e6cb9078
heyuhhh <2468861298@qq.com>
2020-04-14 21:45:32 +0800

    Codeforces Round #633 (Div. 1)

---
+++ oj/codeforces/contests/1338/A.cpp
@@ -0,0 +1,63 @@
+/*
+ * Author:  heyuhhh
+ * Created Time:  2020/4/13 21:32:27
+ */
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <assert.h>
+#define MP make_pair
+#define fi first
+#define se second
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define INF 0x3f3f3f3f
+#define Local
+#ifdef Local
+  #define dbg(args...) do { cout << #args << " -> "; err(args); } while (0)
+  void err() { std::cout << std::endl; }
+  template<typename T, typename...Args>
+  void err(T a, Args...args) { std::cout << a << ' '; err(args...); }
+  template <template<typename...> class T, typename t, typename... A> 
+  void err(const T <t> &arg, const A&... args) {
+  for (auto &v : arg) std::cout << v << ' '; err(args...); }
+#else
+  #define dbg(...)
+#endif
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+//head
+const int N = 1e5 + 5;
+
+int n;
+
+void run() {
+    cin >> n;
+    int last = -INF, Max = -INF;
+    for(int i = 1; i <= n; i++) {
+        int x; cin >> x;   
+        last = max(last, x);
+        Max = max(Max, last - x);
+    }
+    int res = 0;
+    while((1ll << res) - 1 < Max) ++res;
+    cout << res<< '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << fixed << setprecision(20);
+    int T; cin >> T; while(T--)
+    run();
+    return 0;
+}
