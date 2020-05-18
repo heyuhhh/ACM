af3618f06069573c7858bdd22dc8d1fbdbc20ca6
heyuhhh <2468861298@qq.com>
2020-05-18 23:00:36 +0800

    log(n)求第k大元素

--- 模板/数据结构/树状数组.cpp
+++ 模板/数据结构/树状数组.cpp
@@ -12,4 +12,15 @@ struct BIT {
     int query(int l, int r) {
         return query(r) - query(l - 1);   
     }
+    //MAX=2^t < N 
+    int kth(int k) {
+	    int p = 0;
+	    for (int i = MAX >> 1; i; i >>= 1) {
+	        if (c[p + i] < k) {
+	            k -= c[p + i];
+	            p += i;
+	        }
+	    }
+	    return p + 1;
+    } 
 }bit;
