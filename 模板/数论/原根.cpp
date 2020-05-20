49fbf701b8a11b9aa7187d433479777eedd0a7a6
heyuhhh <2468861298@qq.com>
2020-05-20 10:24:33 +0800

    fixed

--- 模板/数论/原根.cpp
+++ 模板/数论/原根.cpp
@@ -1,4 +1,4 @@
-ll qp(ll a, ll b) {
+ll qpow(ll a, ll b, ll p) {
     ll ans = 1;
     while(b) {
         if(b & 1) ans = ans * a % p;
@@ -7,22 +7,25 @@ ll qp(ll a, ll b) {
     }
     return ans;
 }
-int work(ll P) {   //p - 1
+//求模m的原根
+int work(ll P, ll m) {   //P = phi(m)
     vector <int> div;
-    for(int i = 2; 1ll * i * i <= P; i++) {
-        if(P % i == 0) {
+    ll x = P;
+    for(int i = 2; 1ll * i * i <= x; i++) {
+        if(x % i == 0) {
             div.push_back(i);
-            while(P % i == 0) P /= i;
+            while(x % i == 0) x /= i;
         }
     }
-    if(P > 1) div.push_back(P);
+    if(x > 1) div.push_back(x);
     for(int i = 2;; i++) {
         bool ok = true;
         for(auto x : div) {
-            if(qp(i, (p - 1) / x) % p == 1) {
+            if(qpow(i, P / x, m) == 1) {
                 ok = false; break;
             }
         }
         if(ok) return i;
     }
+    return -1;
 } 
