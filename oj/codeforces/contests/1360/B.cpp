f1106009d4500134b22b7665877dbfe821aa185b
heyuhhh <2468861298@qq.com>
2020-05-26 10:56:41 +0800

    Codeforces Round #644 (Div. 3)

---
+++ oj/codeforces/contests/1360/B.cpp
@@ -0,0 +1,63 @@
+/*
+ * Author:  heyuhhh
+ * Created Time:  2020/5/25 9:59:00
+ */
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <assert.h>
+#define MP make_pair
+#define fi first
+#define se second
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define INF 0x3f3f3f3f
+#define Local
+#ifdef Local
+  #define dbg(args...) do { cout << #args << " -> "; err(args); } while (0)
+  void err() { std::cout << std::endl; }
+  template<typename T, typename...Args>
+  void err(T a, Args...args) { std::cout << a << ' '; err(args...); }
+  template <template<typename...> class T, typename t, typename... A> 
+  void err(const T <t> &arg, const A&... args) {
+  for (auto &v : arg) std::cout << v << ' '; err(args...); }
+#else
+  #define dbg(...)
+#endif
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+//head
+const int N = 1e5 + 5;
+
+void run() {
+    int n; cin >> n;
+    vector <int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int d = INF;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i != j) d = min(d, abs(a[i] - a[j]));
+        }
+    }
+    cout << d << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << fixed << setprecision(20);
+    int T; cin >> T; while(T--)
+    run();
+    return 0;
+}
---
+++ oj/codeforces/contests/1360/D.cpp
@@ -0,0 +1,62 @@
+/*
+ * Author:  heyuhhh
+ * Created Time:  2020/5/25 9:42:25
+ */
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <assert.h>
+#define MP make_pair
+#define fi first
+#define se second
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define INF 0x3f3f3f3f
+#define Local
+#ifdef Local
+  #define dbg(args...) do { cout << #args << " -> "; err(args); } while (0)
+  void err() { std::cout << std::endl; }
+  template<typename T, typename...Args>
+  void err(T a, Args...args) { std::cout << a << ' '; err(args...); }
+  template <template<typename...> class T, typename t, typename... A> 
+  void err(const T <t> &arg, const A&... args) {
+  for (auto &v : arg) std::cout << v << ' '; err(args...); }
+#else
+  #define dbg(...)
+#endif
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+//head
+const int N = 1e5 + 5;
+
+void run() {
+    int n, k; cin >> n >> k;
+    vector <int> d;
+    for (int i = 1; 1ll * i * i <= n; i++) {
+        if (n % i == 0) {
+            d.push_back(i);
+            if (i != n / i) d.push_back(n / i);
+        }
+    }
+    sort(all(d));
+    int p = upper_bound(all(d), k) - d.begin() - 1;
+    cout << n / d[p] << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << fixed << setprecision(20);
+    int T; cin >> T; while(T--)
+    run();
+    return 0;
+}
