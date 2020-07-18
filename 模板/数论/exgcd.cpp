490f27f5334951ae29a2793650bc906c20833cd2
heyuhhh <2468861298@qq.com>
2020-07-18 20:44:14 +0800

    修复重大bug

--- 模板/数论/exgcd.cpp
+++ 模板/数论/exgcd.cpp
@@ -1,3 +1,8 @@
+//扩展欧几里得
+//求解最小正整数解
+//x_0,y_0为方程a'x+b'y=c'的解(除以gcd过后) 
+//x=c'*(x_0+k*b'),y=c'*(y_0-k*a')
+//注意最后要乘上c/gcd 
 void exgcd(ll a, ll b, ll &x, ll &y) {
     if(b == 0) {
         x = 1, y = 0;
@@ -8,15 +13,13 @@ void exgcd(ll a, ll b, ll &x, ll &y) {
     x = y;
     y = z - y * (a / b);
 } 
-//求解最小正整数解
-//x=c*x_0+k*b,y=c*y_0-k*a
 ll calc(ll a, ll b, ll c) {
     ll x, y;
     ll g = __gcd(a, b);
     if(c % g != 0) return -1;
     a /= g, b /= g, c /= g;
     exgcd(a, b, x, y);
-    x *= c;
     x = (x % b + b) % b;
+    x *= c; //!!!
     return x;
 }
