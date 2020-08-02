2bc460f7eb4533202df454c79919904be0ea4ac1
heyuhhh <2468861298@qq.com>
2020-08-02 19:33:33 +0800

    Update

--- 模板/数据结构/树链剖分.cpp
+++ 模板/数据结构/树链剖分.cpp
@@ -1,24 +1,36 @@
-void dfs1(int u, int pa, int d) {
-    deep[u] = d;
-    fa[u] = pa;
+// 树链剖分
+// 轻边个数为O(logn)，重链个数为O(logn)，top数组记录的为重链顶点 
+// 注意bson的初始化，其余可以自动初始化
+// 注意每个点实际值为dfn[x]
+// 处理一条链的信息，如果两条链就先跳到一条链上去，然后再操作一次就行 
+int sz[N], deep[N], bson[N], ff[N];
+int top[N], dfn[N], T;
+void dfs(int u, int fa) {
+    deep[u] = deep[fa] + 1;
     sz[u] = 1;
-    int mx = -1;
-    for(int i = head[u]; i != -1; i = e[i].next) {
-        int v = e[i].v;
-        if(v == pa) continue ;
-        dfs1(v, u, d + 1) ;
-        sz[u] += sz[v] ;
-        if(sz[v] > mx) mx = sz[v], son[u] = v;
+    ff[u] = fa;
+    int Max = -1;
+    for (auto v : G[u]) {
+        if (v != fa) {
+            dfs(v, u);
+            sz[u] += sz[v];
+            if (sz[v] > Max) {
+                Max = sz[v];
+                bson[u] = v;
+            }
+        }
     }
 }
-void dfs2(int u, int topf) {
-    id[u] = ++cnt;
-    v[cnt] = w[u] ;
+
+void dfs(int u, int fa, int topf) {
+    dfn[u] = ++T;
     top[u] = topf;
-    if(!son[u]) return ;
-    dfs2(son[u], topf) ;
-    for(int i = head[u]; i != -1; i = e[i].next) {
-        int v = e[i].v;
-        if(v != son[u] && v != fa[u]) dfs2(v, v) ;
+    if (bson[u] != 0) {
+        dfs(bson[u], u, topf);
+    }
+    for (auto v : G[u]) {
+        if (v != fa && v != bson[u]) {
+            dfs(v, u, v);
+        }
     }
 } 
