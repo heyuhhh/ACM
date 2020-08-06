e8b0e2eccda83eed9fd37825e57a72b3f19887e4
heyuhhh <2468861298@qq.com>
2020-08-06 20:37:31 +0800

    Update

--- 模板/数论/杜教筛.cpp
+++ 模板/数论/杜教筛.cpp
@@ -1,3 +1,20 @@
+int mu[N], p[N];
+ll phi[N];
+bool chk[N];
+unordered_map <int, ll> mp1, mp2;
+void init() {
+    mu[1] = phi[1] = 1;
+    int cnt = 0, k = N - 1;
+    for(int i = 2; i <= k; i++) {
+        if(!chk[i]) p[++cnt] = i, mu[i] = -1, phi[i] = i - 1;
+        for(int j = 1; j <= cnt && i * p[j] <= k; j++) {
+            chk[i * p[j]] = 1;
+            if(i % p[j] == 0) {mu[i * p[j]] = 0; phi[i * p[j]] = phi[i] * p[j]; break;}
+            mu[i * p[j]] = -mu[i]; phi[i * p[j]] = phi[i] * (p[j] - 1);
+        }
+    }
+    for(int i = 1; i < N; i++) mu[i] += mu[i - 1], phi[i] += phi[i - 1];
+}
 ll djs_mu(int n) {
     if(n <= 5000000) return mu[n];
     if(mp1[n]) return mp1[n];
