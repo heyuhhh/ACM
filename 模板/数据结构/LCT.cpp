69543c36f9d2d37ae3c17431e590a7a27c98256e
heyuhhh <2468861298@qq.com>
2020-08-18 11:24:25 +0800

    LCT

---
+++ 模板/数据结构/LCT.cpp
@@ -0,0 +1,222 @@
+// LCT 维护路径异或值
+// 虚实相转化的数据结构，配合splay更加灵活与强大
+// 可支持动态添边、删边、换根等操作，也可代替树剖，因为树是动态的
+// 特点：1.认父不认子，与儿子的边可以随便断开，但是父亲始终知道是哪个；
+// 2.中序遍历为树上的一条深度严格递增的实链，每条实链都由一个splay维护；
+// 3.实链虚链可以相互转化，我们自己确定
+struct LCT {
+    #define ls ch[x][0]
+    #define rs ch[x][1]
+    int ch[N][2], fa[N], sum[N], val[N];
+    int tag[N], siz[N];
+    inline void init(int n) {
+        for (int i = 1; i <= n; i++) {
+            ch[i][0] = ch[i][1] = 0;
+            fa[i] = sum[i] = val[i] = 0;
+            tag[i] = siz[i] = 0;
+        }
+    }
+    // 注意pushup在哪里用，一般是进行上旋操作时用到
+    inline void pushup(int x) {
+        siz[x] = siz[ls] + siz[rs] + 1;
+        sum[x] = sum[ls] ^ sum[rs] ^ val[x];
+    }
+    inline void pushdown(int x) {
+        if (tag[x]) {
+            if (ls) swap(ch[ls][0], ch[ls][1]), tag[ls] ^= tag[x];
+            if (rs) swap(ch[rs][0], ch[rs][1]), tag[rs] ^= tag[x];
+            tag[x] = 0;
+        }
+    }
+    // 标记要先下传，避免奇奇怪怪的错误
+    void update(int x) {
+        if (!isroot(x)) update(fa[x]);
+        pushdown(x);
+    }
+    inline int get(int x) {
+        return x == ch[fa[x]][1];
+    }
+    inline bool isroot(int x) {
+        return ch[fa[x]][0] != x && ch[fa[x]][1] != x;
+    }
+    // 与splay类似，多了一个isroot的判断，rotate时会变换父亲
+    inline void rotate(int x) {
+        int y = fa[x], z = fa[y], k = get(x);
+        if (!isroot(y)) ch[z][ch[z][1] == y] = x;
+        ch[y][k] = ch[x][k ^ 1]; fa[ch[y][k]] = y;
+        ch[x][k ^ 1] = y; fa[y] = x; fa[x] = z;
+        pushup(y), pushup(x);
+    }
+    inline void splay(int x) {
+        update(x);
+        for (int f = fa[x]; !isroot(x); f = fa[x]) {
+            if (!isroot(f)) rotate(get(f) == get(x) ? f : x);
+            rotate(x);
+        }
+    }
+    // 核心操作，将x到当前树中的根这条路径上的边全转化为实边，这样x和根节点在一颗splay里面
+    // 返回最后得到splay的根节点，如果连用两次，那么就是两个点的lca
+    inline int access(int x) {
+        int p;
+        for (p = 0; x; p = x, x = fa[x]) {
+            splay(x); ch[x][1] = p; pushup(x);
+        }
+        return p;
+    }
+    // 重要操作，将整棵树的根变为x，实际上就是一段路径的边方向发生翻转，这样才能使得任意一条链都满足深度递增
+    inline void makeroot(int x) {
+        x = access(x); swap(ls, rs);
+        tag[x] ^= 1;
+    }
+    // 链接两个点，如果已联通则不链接
+    inline void link(int x, int y) {
+        makeroot(x), splay(x);
+        makeroot(y), splay(y);
+        if (fa[x] || fa[y]) return; // 如果在一连通块中，那么有个必然不能为根，所以存在父亲
+        fa[y] = x;
+    }
+    // 提取x~y这条链，并且使得y为splay树根
+    inline void split(int x, int y) {
+        makeroot(x); access(y); splay(y);
+    }
+    // 切断一条边，注意不存在边的判断
+    inline void cut(int x, int y) {
+        makeroot(y); access(x); splay(x);
+        if (siz[x] != 2 || ls != y) return;
+        ls = fa[y] = 0;
+    }
+    // 寻找x所在连通块的根节点，深度最小那么一直往左子树走即可，最后会使得根节点为splay的树根
+    inline int find(int x) {
+        access(x), splay(x);
+        while (ls) pushdown(x), x = ls;
+        splay(x); return x;
+    }
+    inline void modify(int x, int v) {
+        splay(x); val[x] = v; pushup(x);
+    }
+    inline int query(int x, int y) {
+        makeroot(x); access(y); splay(y);
+        return sum[y];
+    }
+}lct;
+
+// 乘法、加法标记
+struct LCT {
+    #define ls ch[x][0]
+    #define rs ch[x][1]
+    int ch[N][2], fa[N], sum[N], val[N];
+    int add[N], mul[N], tag[N], siz[N];
+    inline void tagadd(int x, int v) {
+        sum[x] = (sum[x] + 1ll * v * siz[x] % MOD) % MOD;
+        val[x] = (val[x] + v) % MOD;
+        add[x] = (add[x] + v) % MOD;
+    }
+    inline void tagmul(int x, int v) {
+        sum[x] = 1ll * sum[x] * v % MOD;
+        val[x] = 1ll * val[x] * v % MOD;
+        add[x] = 1ll * add[x] * v % MOD;
+        mul[x] = 1ll * mul[x] * v % MOD;
+    }
+    inline void init(int n) {
+        for (int i = 1; i <= n; i++) {
+            ch[i][0] = ch[i][1] = 0;
+            fa[i] = sum[i] = 0;
+            siz[i] = 0;
+            add[i] = 0, mul[i] = val[i] = 1;
+        }
+    }
+    inline void pushup(int x) {
+        siz[x] = siz[ls] + siz[rs] + 1;
+        sum[x] = (sum[ls] + sum[rs] + val[x]) % MOD;
+    }
+    inline void pushdown(int x) {
+        if (tag[x]) {
+            if (ls) swap(ch[ls][0], ch[ls][1]), tag[ls] ^= tag[x];
+            if (rs) swap(ch[rs][0], ch[rs][1]), tag[rs] ^= tag[x];
+            tag[x] = 0;
+        }
+        if (mul[x] != 1) {
+            if (ls) {
+                tagmul(ls, mul[x]);
+            }
+            if (rs) {
+                tagmul(rs, mul[x]);
+            }
+            mul[x] = 1;
+        }
+        if (add[x] > 0) {
+            if (ls) {
+                tagadd(ls, add[x]);
+            }
+            if (rs) {
+                tagadd(rs, add[x]);
+            }
+            add[x] = 0;
+        }
+    }
+    // 对于一个连通块，下放懒标记
+    void update(int x) {
+        if (!isroot(x)) update(fa[x]);
+        pushdown(x);
+    }
+    inline int get(int x) {
+        return x == ch[fa[x]][1];
+    }
+    inline bool isroot(int x) {
+        return ch[fa[x]][0] != x && ch[fa[x]][1] != x;
+    }
+    inline void rotate(int x) {
+        int y = fa[x], z = fa[y], k = get(x);
+        if (!isroot(y)) ch[z][ch[z][1] == y] = x;
+        ch[y][k] = ch[x][k ^ 1]; fa[ch[y][k]] = y;
+        ch[x][k ^ 1] = y; fa[y] = x; fa[x] = z;
+        pushup(y), pushup(x);
+    }
+    // 每次splay时都会提前下传上面连通块的懒标记，然后虚实转化就没问题
+    inline void splay(int x) {
+        update(x);
+        for (int f = fa[x]; !isroot(x); f = fa[x]) {
+            if (!isroot(f)) rotate(get(f) == get(x) ? f : x);
+            rotate(x);
+        }
+    }
+    inline int access(int x) {
+        int p;
+        for (p = 0; x; p = x, x = fa[x]) {
+            splay(x); ch[x][1] = p; pushup(x);
+        }
+        return p;
+    }
+    inline void makeroot(int x) {
+        x = access(x); swap(ls, rs);
+        tag[x] ^= 1;
+    }
+    inline void link(int x, int y) {
+        makeroot(x), splay(x);
+        makeroot(y), splay(y);
+        if (fa[x] || fa[y]) return; 
+        fa[y] = x;
+    }
+    inline void split(int x, int y) {
+        makeroot(x); access(y); splay(y);
+    }
+    inline void cut(int x, int y) {
+        makeroot(y); access(x); splay(x);
+        if (siz[x] != 2 || ls != y) return;
+        ls = fa[y] = 0;
+    }
+    inline int find(int x) {
+        access(x), splay(x);
+        while (ls) pushdown(x), x = ls;
+        splay(x); return x;
+    }
+    inline int query(int x, int y) {
+        makeroot(x); access(y); splay(y);
+        return sum[y];
+    }
+    void update(int x, int y, int op, int v) {
+        split(y, x);
+        if (op) tagmul(x, v);
+        else tagadd(x, v);
+    }
+}lct;
