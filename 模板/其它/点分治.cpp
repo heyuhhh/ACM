141c52e9a1b5896b3046fd7916523c9df869cdcc
heyuhhh <2468861298@qq.com>
2020-08-23 12:38:48 +0800

    点分治

--- 模板/其它/点分治.cpp
+++ 模板/其它/点分治.cpp
@@ -1,81 +1,66 @@
-/*
-	ÓÅÑÅµÄ±©Á¦£¬¶ÔÓÚÒ»¿ÅÎŞ¸ùÊ÷Ã¿´ÎÒÔÊ÷µÄÖØĞÄÎª¸ùÈİ³â¼ÆËã´ğ°¸£¬±¾ÖÊÏàµ±ÓÚÃ¶¾ÙÃ¿Ò»¸ö½áµã×÷ÎªÂ·¾¶ÉÏÃæµÄ±Ø¾­µãÈ»ºóÀ´¿¼ÂÇ¡£
-	¸´ÔÓ¶ÈÒ»°ãÎª$O(nlogn)$¡£
-	
-	Ñ°ÕÒÁ½µãÖ®¼äÂ·¾¶Îª$3$µÄ±¶ÊıµÄ´úÂë£º
-*/
-#include <bits/stdc++.h>
-#define MP make_pair
-#define fi first
-#define se second
-#define INF 0x3f3f3f3f
-using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-const int N = 20005;
-int n;
-vector <pii> g[N];
-int sz[N], mx[N], tsz;
-int d[N];
-int rt, ans, cnt;
+// ä¸è¦å¿˜äº†dfså‰å…ˆfindrootä¸€æ¬¡
+int n, m;
+vector <pii> G[N];
+int tsz, rt;
+int sz[N], Max[N], father[N];
 bool vis[N];
 void getrt(int u, int fa) {
-    sz[u] = 1; mx[u] = 0;
-    for(auto it : g[u]) {
-        int v = it.fi;
+    sz[u] = 1; Max[u] = 0;
+    for(auto it : G[u]) {
+        int v = it.fi, w = it.se;
         if(v == fa || vis[v]) continue;
         getrt(v, u);
         sz[u] += sz[v];
-        if(sz[v] > mx[u]) mx[u] = sz[v];
+        if(sz[v] > Max[u]) Max[u] = sz[v];
     }
-    mx[u] = max(mx[u], tsz - sz[u]);
-    if(mx[u] < mx[rt]) rt = u;
+    Max[u] = max(Max[u], tsz - sz[u]);
+    if(Max[u] < Max[rt]) rt = u;
 }
-void dfs2(int u, int D, int fa) {
-    d[++cnt] = D;
-    for(auto it : g[u]) {
+void findrt(int u, int fa) {
+    tsz = (sz[u] == 0 ? n : sz[u]);
+    Max[rt = 0] = INF;
+    getrt(u, fa);
+}
+void dfs(int u, int fa) {
+    vis[u] = true;
+    for(auto it : G[u]) {
+        int v = it.fi;
+        if(v != fa && !vis[v]) {
+            findrt(v, u);
+            father[rt] = u;
+            dfs(rt, 0);
+        }
+    }   
+}
+
+int f[N][20], deep[N], d[N];
+void dfs2(int u, int fa) {
+    deep[u] = deep[fa] + 1;
+    f[u][0] = fa;   
+    for(int i = 1; i < 20; i++) {
+        f[u][i] = f[f[u][i - 1]][i - 1];
+    }    
+    for(auto it : G[u]) {
         int v = it.fi, w = it.se;
-        if(vis[v] || v == fa) continue;
-        dfs2(v, D + w, u);
+        if(v != fa) {
+            d[v] = d[u] + w;
+            dfs2(v, u);
+        }
     }
 }
-int calc() {
-    for(int i = 1; i <= cnt; i++) d[i] %= 3;
-    int tmp[3] = {0, 0, 0};
-    for(int i = 1; i <= cnt; i++) ++tmp[d[i]];
-    int ans = tmp[0] * (tmp[0] - 1) + 2 * (tmp[1] * tmp[2]);
-    return ans;
-}
-void dfs(int u) {
-    vis[u] = 1;
-    cnt = 0; dfs2(u, 0, 0);
-    int tmp = calc();
-    ans += tmp;
-    for(auto it : g[u]) {
-        int v = it.fi, w = it.se;
-        if(vis[v]) continue;
-        cnt = 0, dfs2(v, w, 0);
-        ans -= calc();
-        tsz = sz[v], rt = 0, getrt(v, u);
-        dfs(rt);
+int LCA(int x, int y) {
+    if(deep[x] < deep[y]) swap(x, y);
+    for(int i = 19; i >= 0; i--) {
+        if(deep[f[x][i]] >= deep[y]) x = f[x][i];
+    }   
+    if(x == y) return x;
+    for(int i = 19; i >= 0; i--) {
+        if(f[x][i] != f[y][i]) x = f[x][i], y = f[y][i];   
     }
+    return f[x][0];
 }
-int gcd(int a, int b) {
-    return b == 0 ? a : gcd(b, a % b);
-}
-int main() {
-    ios::sync_with_stdio(false); cin.tie(0);
-    cin >> n;
-    for(int i = 1; i < n; i++) {
-        int u, v, w; cin >> u >> v >> w;
-        g[u].push_back(MP(v, w)); g[v].push_back(MP(u, w));
-    }
-    tsz = n, mx[0] = INF, getrt(1, 0);
-    dfs(rt);
-    ans += n;
-    int tot = n * n;
-    int g = gcd(tot, ans);
-    ans /= g, tot /= g;
-    cout << ans << '/' << tot;
-    return 0;
-}
+ 
+int dis(int x, int y) {
+    int z = LCA(x, y);
+    return d[x] + d[y] - 2 * d[z];   
+} 
