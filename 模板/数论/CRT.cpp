7fa7c25520912ec251af2e53203e00ccbe271227
heyuhhh <2468861298@qq.com>
2020-10-02 14:27:14 +0800

    Update

--- 模板/数论/CRT.cpp
+++ 模板/数论/CRT.cpp
@@ -1,3 +1,5 @@
+// 求解一元一次同余方程组
+// 当模数不互质时，两两的答案可以通过exgcd解方程解得
 struct CRT{
     void exgcd(ll a, ll b, ll &g, ll &x, ll &y) {
         if(b == 0) {
@@ -9,6 +11,7 @@ struct CRT{
         x = y;
         y = t - (a / b) * y;
     }
+    // m：模数；a：模意义下的答案；n：项数
     ll china(int m[], int a[], int n) {
         ll M, Mi, d, X, Y, ans;
         M = 1; ans = 0;
