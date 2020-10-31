a009bbd5486b5c58edf916e13091c141f92d646b
heyuhhh <2468861298@qq.com>
2020-10-31 09:35:43 +0800

    更新扩展crt

--- 模板/数论/CRT.cpp
+++ 模板/数论/CRT.cpp
@@ -25,3 +25,31 @@ struct CRT{
         return ans;
     }
 }crt; 
+
+// 扩展中国剩余定理，通过exgcd两两合并答案，两个方程合并为一个方程
+// 注意M=lcm(m_1,m_2)，相当于通解为 x+kM
+void exgcd(ll a, ll b, ll &g, ll &x, ll &y) {
+    if(b == 0) {
+        x = 1, y = 0, g = a;
+        return ;
+    }
+    exgcd(b, a % b, g, x, y);
+    int t = x;
+    x = y;
+    y = t - (a / b) * y;
+}
+
+ll CRT(const vector<int>& a, const vector<int>& m) {
+    assert(sz(a) == sz(m));
+    ll x0 = a[0], m0 = m[0], n = sz(a);
+    for (int i = 1; i < n; i++) {
+        ll x1 = a[i], m1 = m[i], x, y, d;
+        exgcd(m0, m1, d, x, y);
+        if ((x0 - x1) % d) return -1;
+        x = (x1 - x0) / d * x % (m0 / d * m1);
+        x0 = (x * m0 + x0) % (m0 / d * m1);
+        m0 = m0 / d * m1;
+        if (x0 < 0) x0 += m0;
+    }
+    return x0;
+}
